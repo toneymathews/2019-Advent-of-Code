008463d59039305a640ce48ac24bae5a0fc543da
toneymathews <toneymathews@gmail.com>
2020-01-02 23:13:24 -0500

    day2 c1 solved

--- day2c1.rb
+++ day2c1.rb
@@ -3,7 +3,7 @@ def process_opcode(array)
 	array[2] = 2
 	position = 0
 	i = 0
-	while position < array.length/4
+	while position <= (array.length)/4
 		if array[i] == 1
 			array[array[i+3]] = array[array[i+1]] + array[array[i+2]]
 		elsif array[i] == 2
@@ -20,7 +20,7 @@ end
 def get_input
 	array = []
 	input = gets.chomp
-	input.split(",").each { |a| array.push(a.sub(",",""))}
+	input.split(",").each { |a| array.push(a.sub(",","").to_i)}
 	array
 end
 puts process_opcode(get_input)
