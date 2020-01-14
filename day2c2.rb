d1791b2a599974daa74c2d084558821a240e82c6
toneymathews <toneymathews@gmail.com>
2020-01-14 13:01:32 -0500

    still working

--- day2c2.rb
+++ day2c2.rb
@@ -1,6 +1,6 @@
 def process_opcode(array, noun, verb)
 	array[1] = noun
-	array[2] = verb
+  array[2] = verb
 	position = 0
 	i = 0
 	while position <= (array.length)/4
@@ -9,8 +9,8 @@ def process_opcode(array, noun, verb)
 		elsif array[i] == 2
 			array[array[i+3]] = array[array[i+1]] * array[array[i+2]]
 		elsif array[i] == 99
-      puts array[0]
       if array[0] == 19690720
+        puts "condition met"
         return array[0]
       else
         return 0
@@ -32,9 +32,15 @@ def find_noun_and_verb(array)
   noun = 0
   verb = 0
   array_backup = array
-  while noun <= 99
-    while verb <= 99
-      array = array_backup
+  puts "length of array is #{array_backup.length}"
+  a = array
+  puts a[0]
+  while noun <= 2
+    verb = 0
+    while verb <= 2
+      #array = array_backup
+      puts a[10]
+      puts noun.to_s + " some " +  verb.to_s + " first element " + array[0].to_s + "backup" + a[0].to_s
       if process_opcode(array, noun, verb) != 0
         puts "value is #{100 * noun + verb}"
       end
